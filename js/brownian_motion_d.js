59cdd0cbe76cd8dd8bbb41e622a3acf90b77bc0a
Brennan <brennanhilton@gmail.com>
2019-11-06 14:23:50 -0500

    removed flex dashboard

---
+++ js/brownian_motion_d.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#brownian_motion_d').scianimator({
+	    'images': ['images/brownian_motion_d1.png', 'images/brownian_motion_d2.png', 'images/brownian_motion_d3.png', 'images/brownian_motion_d4.png', 'images/brownian_motion_d5.png', 'images/brownian_motion_d6.png', 'images/brownian_motion_d7.png', 'images/brownian_motion_d8.png', 'images/brownian_motion_d9.png', 'images/brownian_motion_d10.png', 'images/brownian_motion_d11.png', 'images/brownian_motion_d12.png', 'images/brownian_motion_d13.png', 'images/brownian_motion_d14.png', 'images/brownian_motion_d15.png', 'images/brownian_motion_d16.png', 'images/brownian_motion_d17.png', 'images/brownian_motion_d18.png', 'images/brownian_motion_d19.png', 'images/brownian_motion_d20.png', 'images/brownian_motion_d21.png', 'images/brownian_motion_d22.png', 'images/brownian_motion_d23.png', 'images/brownian_motion_d24.png', 'images/brownian_motion_d25.png', 'images/brownian_motion_d26.png', 'images/brownian_motion_d27.png', 'images/brownian_motion_d28.png', 'images/brownian_motion_d29.png', 'images/brownian_motion_d30.png', 'images/brownian_motion_d31.png', 'images/brownian_motion_d32.png', 'images/brownian_motion_d33.png', 'images/brownian_motion_d34.png', 'images/brownian_motion_d35.png', 'images/brownian_motion_d36.png', 'images/brownian_motion_d37.png', 'images/brownian_motion_d38.png', 'images/brownian_motion_d39.png', 'images/brownian_motion_d40.png', 'images/brownian_motion_d41.png', 'images/brownian_motion_d42.png', 'images/brownian_motion_d43.png', 'images/brownian_motion_d44.png', 'images/brownian_motion_d45.png', 'images/brownian_motion_d46.png', 'images/brownian_motion_d47.png', 'images/brownian_motion_d48.png', 'images/brownian_motion_d49.png', 'images/brownian_motion_d50.png'],
+	    'width': 480,
+	    'delay': 500,
+	    'loopMode': 'loop'
+	});
+	$('#brownian_motion_d').scianimator('play');
+    });
+})(jQuery);
